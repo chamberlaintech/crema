f8e3853f179dad589f9e6565ff24bc20f142a83c
chamberlaintech <chamberlainwebtech@gmail.com>
2025-01-26 13:04:21 +0100

    scrolling change

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Error from "./pages/Error/Error";
 import Footer from "./components/Footer/Footer";
+import ScrollTop from "./components/ScrollTop/ScrollTop";
 
 function App() {
   return (
     <div className="flex-wrapper">
       <Navbar />
+      <ScrollTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
